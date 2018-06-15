f285157b40428425c6819a97daabcb1cf4f7c038
Ador <adroit.ador@live.com>
2018-06-15 15:30:36 +0600

    Fixed Credits.

--- config.sh
+++ config.sh
@@ -49,11 +49,11 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "--------------------------------"
+  ui_print "================================"
   ui_print "      Energized Protection      "
   ui_print "--------------------------------"
   ui_print "    ad.porn.malware blocking.   "
-  ui_print "        Magisk All-In-One       "
+  ui_print "       Magisk All-In-One        "
   ui_print "--------------------------------"
 }
 
