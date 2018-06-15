6e030d57511c797ddbc67ceb4abb309948e8d764
Ador <adroit.ador@live.com>
2018-06-15 15:27:32 +0600

    Fixed Config.sh UI_Prints.

--- config.sh
+++ config.sh
@@ -49,10 +49,12 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Unified Hosts Adblock     "
-  ui_print "          (Zackptg5)           "
-  ui_print "*******************************"
+  ui_print "--------------------------------"
+  ui_print "      Energized Protection      "
+  ui_print "--------------------------------"
+  ui_print "    ad.porn.malware blocking.   "
+  ui_print "        Magisk All-In-One       "
+  ui_print "--------------------------------"
 }
 
 ##########################################################################################
